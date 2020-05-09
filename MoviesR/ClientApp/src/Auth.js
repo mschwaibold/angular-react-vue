c3f96c864c3d84a74e492f290571d2457f4c3f7a
Max Schwaibold <max.schwaibold@gmx.net>
2020-05-09 16:50:14 +0200

    Add missing files in the react app?

---
+++ MoviesR/ClientApp/src/Auth.js
@@ -0,0 +1,50 @@
+﻿///////////////////////////////////////
+//                                   //
+//    Author: Max Schwaibold         //
+//    Date:   07.05.2020             //
+//                                   //
+///////////////////////////////////////
+
+import { UserManager } from 'oidc-client';
+
+export class Auth {
+
+  constructor() {
+    var config = {
+      authority: "https://localhost:44301",
+      client_id: "react",
+      redirect_uri: "https://localhost:44303/oidc-callback",
+      response_type: "code",
+      scope: "openid profile movieApi",
+      post_logout_redirect_uri: "https://localhost:44303"
+    };
+
+    this.userManager = new UserManager(config);
+    this.userManager.getUser().then(user => this._user = user);
+
+    this.login = this.login.bind(this);
+    this.logout = this.logout.bind(this);
+    this.getAccessToken = this.getAccessToken.bind(this);
+    this.isLoggedIn = this.isLoggedIn.bind(this);
+  }
+
+  login() {
+    this.userManager.signinRedirect();
+  }
+
+  logout() {
+    this.userManager.signoutRedirect();
+  }
+
+  getAccessToken() {
+    return this.userManager.getUser().then(user => {
+      return user ? user.access_token : null;
+    });
+  }
+
+  isLoggedIn() {
+    return this.userManager.getUser().then(user => {
+      return !!user && !user.expired;
+    });
+  }
+}
---
+++ MoviesR/ClientApp/src/components/OidcCallback.js
@@ -0,0 +1,35 @@
+﻿///////////////////////////////////////
+//                                   //
+//    Author: Max Schwaibold         //
+//    Date:   07.05.2020             //
+//                                   //
+///////////////////////////////////////
+
+import React, { Component } from 'react';
+import { UserManager } from 'oidc-client';
+
+export class OidcCallback extends Component {
+  static displayName = OidcCallback.name;
+
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    this.completeLogin();
+  }
+
+  completeLogin() {
+    new UserManager({ response_mode: "query" }).signinRedirectCallback().then(function () {
+      window.location.href = "/";
+    }).catch(function (e) {
+      console.error(e);
+    });
+  }
+
+  render() {    
+    return (
+      <h1>Completing login. This may take some seconds...</h1>
+    )
+  }
+}
